4ab45f02e132c1b5d0e5bf1a1bf96d12c5a277fc
EP <ep@github.com>
2023-02-21 14:05:53 -0500

    added tabs and date picker

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import "react-datepicker/dist/react-datepicker.css";
+import { useEffect, useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import ReactDatePicker from 'react-datepicker';
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
   const fetchData = async () => {
     try {
       const response = await fetch(`https://lo-interview.s3.us-west-2.amazonaws.com/health_sessions.json`);
@@ -11,28 +16,43 @@ function App() {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
+    console.log('useEffect')
     fetchData();
-  }, [])
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <Tabs>
+          <TabList>
+            <Tab>Day-by-day Overview</Tab>
+            <Tab>Distributions of Session Timing</Tab>
+            <Tab>Custom</Tab>
+          </TabList>
+
+          <TabPanel className='tab-panel'>
+            <div className='datepicker-container'>
+              <ReactDatePicker
+                wrapperClassName='date-picker-wrapper'
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                minDate={new Date('2015/02/01')}
+                maxDate={new Date('2015/02/28')}
+                placeholderText='Select Day'
+              />
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 3</h2>
+          </TabPanel>
+        </Tabs>   
+      </div>
     </div>
   );
 }
