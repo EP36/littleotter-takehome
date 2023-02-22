f0960a19ea28b200f0b086cd4642e73713e6db15
EP <ep@github.com>
2023-02-21 20:17:07 -0500

    date filtered logic implemented

--- src/App.js
+++ src/App.js
@@ -7,21 +7,39 @@ import ReactDatePicker from 'react-datepicker';
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(null);
+  const [sessions, setSessions] = useState([])
 
   const fetchData = async () => {
     try {
       const response = await fetch(`https://lo-interview.s3.us-west-2.amazonaws.com/health_sessions.json`);
       const data = await response.json();
-      console.log('this is data', data);
+      setSessions(data);
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    console.log('useEffect')
     fetchData();
   }, []);
+
+  useEffect(() => {
+    console.log('selected date => ', selectedDate, new Date(selectedDate).getTime())
+    let filtered = sessions.filter(session => new Date(session.start_time).getTime() >= new Date(selectedDate).getTime() && new Date(session.start_time).getTime() <= (new Date(selectedDate).getTime() + 86400000));
+    // 86400000
+
+    console.log('sessions => ', sessions)
+    console.log('filtered => ', filtered)
+  })
+
+  // for a selected day, show a simple snapshot of the following data:
+    // i. number of sessions
+    // ii. average length of session
+    // iii. average distance traveled by patient
+    // iv. average age of patient
+
+
+  // filter data by selected day by having start_time < selectedDate OR stop_time < selectedDate + 1
 
   return (
     <div className='App'>
