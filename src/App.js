d7beb73809ea5fc6c3cfbafc611fbf3a11d7aeea
EP <ep@github.com>
2023-02-21 20:38:31 -0500

    number of sessions implemented

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import "react-datepicker/dist/react-datepicker.css";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import ReactDatePicker from 'react-datepicker';
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(null);
-  const [sessions, setSessions] = useState([])
+  const [initialSessions, setInitialSessions] = useState([]);
+  const [sessions, setSessions] = useState([]);
+  const [totalSessions, setTotalSessions] = useState(null);
 
   const fetchData = async () => {
     try {
       const response = await fetch(`https://lo-interview.s3.us-west-2.amazonaws.com/health_sessions.json`);
       const data = await response.json();
-      setSessions(data);
+      setInitialSessions(data);
     } catch (error) {
       console.error(error);
     }
@@ -22,15 +24,34 @@ function App() {
   useEffect(() => {
     fetchData();
   }, []);
-
+  
+  useEffect(() => {
+    sessions.length > 0 ? setTotalSessions(sessions.length) : setTotalSessions(initialSessions.length);
+  }, [initialSessions, sessions])
+  
+  useEffect(() => {
+    console.log('hellllloooo', filteredSessions)
+    setSessions(filteredSessions);
+  }, [selectedDate])
+  
   useEffect(() => {
     console.log('selected date => ', selectedDate, new Date(selectedDate).getTime())
-    let filtered = sessions.filter(session => new Date(session.start_time).getTime() >= new Date(selectedDate).getTime() && new Date(session.start_time).getTime() <= (new Date(selectedDate).getTime() + 86400000));
-    // 86400000
-
+    
     console.log('sessions => ', sessions)
-    console.log('filtered => ', filtered)
   })
+  
+  const filteredSessions = useMemo(() => {
+    // 86400000 is extra ms to next day
+    return initialSessions.filter(session => new Date(session.start_time).getTime() >= new Date(selectedDate).getTime() && new Date(session.start_time).getTime() <= (new Date(selectedDate).getTime() + 86400000));
+  }, [sessions, selectedDate])
+
+  const calcSum = (nums, key) => {
+    return nums.reduce((partialSum, a) => partialSum + a[key], 0);
+  }
+
+  const calcAvg = (nums, key) => {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+  }
 
   // for a selected day, show a simple snapshot of the following data:
     // i. number of sessions
@@ -61,6 +82,12 @@ function App() {
                 maxDate={new Date('2015/02/28')}
                 placeholderText='Select Day'
               />
+            </div>
+            <div className='overview-container'>
+              <h3>Number of sessions: {totalSessions}</h3>
+              <h3>Average length of session: </h3>
+              <h3>Average distance traveled by patient: </h3>
+              <h3>Average age of patient: </h3>
             </div>
           </TabPanel>
           <TabPanel>
